e8096ff2314da9ff999eb9b07f85aa9d7c31451b
Bushra178 <74563436+Bushra178@users.noreply.github.com>
2023-09-24 12:13:23 -0700

    Delete terraform directory

--- terraform/terra-ec2-module/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2-public-ip" {
-    value = module.myapp-server.instance.public_ip
-}
--- terraform/terra-ec2-module/variables.tf
+++
@@ -1,8 +0,0 @@
-variable subnet_cidr_block {}
-variable vpc_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable image_name {}
-variable ssh-key-private {}
